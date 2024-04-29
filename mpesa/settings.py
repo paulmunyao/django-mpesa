3550096be6f8667598b8a7522ee32e4e533d573b
paulmunyao <paulmunyao094@gmail.com>
2024-04-29 15:45:50 +0300

    run ngrok

--- mpesa/settings.py
+++ mpesa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5$gp45y=xj+sn!=qb45la=^%2g!c4usi4treno@+t-vyjnxx*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io']
 
 
 # Application definition
